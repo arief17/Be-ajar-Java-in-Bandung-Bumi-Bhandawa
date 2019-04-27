5f6d122096caee018821878cbfc2f660a9369022
arief17 <arif.fahrudin17@gmail.com>
2019-04-27 13:33:07 +0700

    javascript awal

---
+++ script.js
@@ -0,0 +1,79 @@
+document.getElementById('heading').innerHTML = "Good JOB!!"
+var name = "Dyo";
+console.log(name);
+var lastName = "Bumi";
+console.log(lastName);
+var age = 26;
+console.log(age);
+var fullName = name + lastName; // concatenation.
+console.log(fullName);
+var job = "Trainer";
+console.log(job);
+
+var bio = "My name " + name + " " + lastName + " " + " my age " + age + " my job " + job;
+console.log(bio);
+
+var x = 5;
+var y = 7;
+var z = x + y;
+
+console.log(z);
+var a = 5;
+var b = 7;
+var c = a * b;
+console.log(c);
+
+
+text1 = "Dyo";
+text2 = "Bumi";
+text3 = text1 + " " + text2;
+console.log(text3);
+document.getElementById('job').innerHTML = c;
+x = 5 + 5;
+y = "5" + 5;
+z = "Hello" + 5;
+console.log(x);
+console.log(y);
+console.log(z);
+
+var name = "Brandon";
+var age = 16;
+var isYoung = "Yes";
+
+if (isYoung == "no") {
+	
+	console.log(name + " is young man")
+	
+} else {
+
+	console.log("I hope he mature soon")
+
+}
+
+var job = 'Trainer'
+
+switch (job) {
+	case 'Trainer':
+		console.log('Dyo adalah Trainer');
+		break;
+	case 'Driver':
+		console.log('Dyo bukan driver');
+		break;
+	case 'Police':
+		console.log('Dyo bukan polisi');
+		break;
+	default:
+		console.log('Not at all');
+}
+
+//job = prompt("Apa profesi Dyo?")
+
+berita="selamat datang di smkn 1 kragilan"
+document.getElementById('berita').innerHTML = berita;
+
+var x = 1;
+console.log(Boolean(x));
+
+//prompt("What is your name")
+
+
